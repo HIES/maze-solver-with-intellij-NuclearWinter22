e8280b4f0c0b05671f50b769b8719002974c1007
NuclearWinter22 <wilsonja@bearsmail.org>
2017-11-13 07:39:15 -0500

    Final Comit

--- src/Maze.java
+++ src/Maze.java
@@ -1,8 +1,14 @@
-public class Maze {
+import java.awt.Color;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Maze_With_Extensions {
     private Cell[][] board;
     private final int DELAY = 200;
 
-    public Maze(int rows, int cols, int[][] map) {
+    public Maze_With_Extensions(int rows, int cols, int[][] map) {
         StdDraw.setXscale(0, cols);
         StdDraw.setYscale(0, rows);
         board = new Cell[rows][cols];
@@ -21,15 +27,16 @@ public class Maze {
                 StdDraw.setPenColor(cell.getColor());
                 StdDraw.filledSquare(cell.getX(), cell.getY(), cell.getRadius());
             }
-            StdDraw.show();
+        StdDraw.show();
     }
 
     public boolean findPath(int row, int col) {
         boolean isComplete = false;
         if (isValid(row, col)) {
-            board[row][col].visitCell();
+            board[row][col].setColor(Color.BLUE);
             draw();
             StdDraw.pause(DELAY);
+            board[row][col].visitCell();
             if (isExit(row, col)) isComplete = true;
             else if (findPath(row, col+1) || findPath(row, col-1) || findPath(row+1, col) || findPath(row-1, col)) isComplete = true;
         }
@@ -51,19 +58,27 @@ public class Maze {
         return row == board.length - 1 && col == board[0].length - 1;
     }
 
-    public static void main(String[] args) {
-        StdDraw.enableDoubleBuffering();
-        int[][] maze = {{1,1,0,0,0,0,0,0,0,0},
-                        {0,1,1,1,1,0,1,1,1,0},
-                        {0,1,1,1,1,0,1,1,0,0},
-                        {0,1,0,1,1,1,1,1,1,0},
-                        {0,1,0,0,0,0,0,1,1,0},
-                        {0,1,1,0,1,1,1,1,1,0},
-                        {0,0,1,0,0,1,0,1,0,0},
-                        {0,1,1,0,1,1,0,1,1,0},
-                        {0,1,1,0,1,1,0,1,1,0},
-                        {0,0,0,0,0,0,0,0,1,1}};
-        Maze geerid = new Maze(maze.length, maze[0].length, maze);
+    private static int[][] loadMaze(String fileName) throws Exception {
+        ArrayList<String[]> mazeRows = new ArrayList<>();
+        File mazeFile = new File("input/"+ fileName + ".csv");
+        Scanner inputObject = new Scanner(mazeFile);
+        while (inputObject.hasNextLine()) {
+            String [] row = inputObject.nextLine().split(",");
+            mazeRows.add(row);
+        }
+        inputObject.close();
+        int[][] maze = new int[mazeRows.size()][mazeRows.get(0).length];
+        for (int i = 0; i < maze.length; i++) {
+            for (int y = 0; y < maze[0].length; y++) {
+                maze[i][y] = Integer.parseInt(mazeRows.get(i)[y]);
+            }
+        }
+        return maze;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[][] maze = loadMaze(args[0]);
+        Maze_With_Extensions geerid = new Maze_With_Extensions(maze.length, maze[0].length, maze);
         geerid.draw();
         geerid.findPath(0, 0);
         geerid.draw();
